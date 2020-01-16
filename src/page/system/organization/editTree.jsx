3c1f666f0fd8ffaceb641775fabf8f5437620884
songziyu <1451664712@qq.com>
2020-01-16 22:14:04 +0800

    1

--- src/page/system/organization/editTree.jsx
+++ src/page/system/organization/editTree.jsx
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import './index.less'
-import {Tree, Icon, Modal} from 'antd'
+import {Tree, Icon, Modal, Input, Form} from 'antd'
 import {reqListChildrenOrg} from "../../../api/organization"
-import Trees from "../role";
 
 const {TreeNode} = Tree;
+const {Search} = Input;
+const {Item} = Form
 
 class EditTree extends Component {
 
     state = {
-        showStatus: 0
+        showStatus: 0,
+        expandedKeys: []
     }
 
     // 组织机构树
@@ -20,8 +22,8 @@ class EditTree extends Component {
                 {
                     item.level == 0 ?
                         (<Icon
-                            type="file-add"
-                            style={{color: '#1570C4'}}
+                            type='plus'
+                            // style={{color: '#1570C4'}}
                             onClick={this.addForm}
                         />) :
                         null
@@ -30,13 +32,13 @@ class EditTree extends Component {
         )
         if (item.children && item.children.length > 0) {
             return (
-                <TreeNode title={title} key={item.id} icon={<Icon type="carry-out" />}>
+                <TreeNode title={title} key={item.id} icon={<Icon type="carry-out"/>}>
                     {this.getTreeNodes(item.children)}
                 </TreeNode>
             )
         } else {
             return (
-                <TreeNode title={title} key={item.id} icon={<Icon type="carry-out" />}/>
+                <TreeNode title={title} key={item.id} icon={<Icon type="carry-out"/>}/>
             )
         }
     })
@@ -57,6 +59,7 @@ class EditTree extends Component {
     handleCancel = () => {
         this.setState({showStatus: 0})
     }
+
     // 点击树节点
     onSelect = async (selectedKeys) => {
         const result = await reqListChildrenOrg(selectedKeys[0])
@@ -78,8 +81,10 @@ class EditTree extends Component {
 
     render() {
         const {showStatus} = this.state
+        const {getFieldDecorator} = this.props.form;
         return (
             <div>
+                <Search style={{marginBottom: 8}} placeholder="Search" onChange={this.onChange}/>
                 <Tree
                     showLine={true}
                     onSelect={this.onSelect}
@@ -97,10 +102,20 @@ class EditTree extends Component {
                     onOk={this.addOrg}
                     onCancel={this.handleCancel}
                 >
+                    <Form>
+                        <Item label="机构代码">
+                            {getFieldDecorator('code', {
+                                rules: [{required: true, message: '请输入机构代码'}],
+                            })(
+                                <Input placeholder="机构代码"/>,
+                            )}
+                        </Item>
+                    </Form>
                 </Modal>
             </div>
         )
     }
 }
 
-export default EditTree
+const WrappedNormalTree = Form.create({name: 'normal_login'})(EditTree);
+export default WrappedNormalTree
